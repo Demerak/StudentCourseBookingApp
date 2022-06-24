d930bcdfccb3658a04c5026a0ddd1c850adc1856
Demerak <alexandre_poirier@outlook.com>
2022-06-24 16:45:50 -0400

    Started thinking about Junit test

---
+++ app/src/androidTest/java/com/example/studentcoursebookingapp/MainActivityTest.java
@@ -0,0 +1,24 @@
+package com.example.studentcoursebookingapp;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testLogin() {
+        //onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+}
+
