65df9d97f54b4ab42317a15ebfb93ac59c55b337
Demerak <alexandre_poirier@outlook.com>
2022-07-04 21:08:02 -0400

    Students can now search for a course by course Id or by name. Doesn't have to be the full name, it can also be a substring of the full course name.

--- app/src/main/java/com/example/studentcoursebookingapp/SelectCourseStudentActivity.java
+++ app/src/main/java/com/example/studentcoursebookingapp/SelectCourseStudentActivity.java
@@ -10,63 +10,89 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.Source;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SelectCourseStudentActivity extends AppCompatActivity {
 
-    private Button homeBtn;
+    private Button homeBtn, filterBtn;
+    private EditText searchText;
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
     private ArrayList<Course> courseListNotEnroll;
-    private ArrayList<Course> courseListEnroll;
-    private RecyclerView recyclerViewEnroll;
-    private RecyclerView recyclerViewUnEnroll;
-    private CourseAdapterEnroll courseAdapterEnroll;
-    private CourseAdapterUnEnroll courseAdapterUnEnroll;
+    private ArrayList<Course> courseListEnrolledIn;
+    private RecyclerView recyclerViewAvailableCourses;
+    private RecyclerView recyclerViewCourseEnrolledIn;
+    private CourseAdapterEnroll courseAdapterAvailableCourses;
+    private CourseAdapterUnEnroll courseAdapterCourseEnrolledIn;
     private List<String> courseEnrollList;
+    private Spinner dayOfWeekSpinner;
+    private ArrayAdapter<String> spinnerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_course_student);
 
-        recyclerViewEnroll = findViewById(R.id.recyclerViewerStudSelectEnroll);
-        recyclerViewEnroll.setHasFixedSize(true);
-        recyclerViewEnroll.setLayoutManager(new LinearLayoutManager((this)));
+        recyclerViewAvailableCourses = findViewById(R.id.recyclerViewerStudAvailableCourse);
+        recyclerViewAvailableCourses.setHasFixedSize(true);
+        recyclerViewAvailableCourses.setLayoutManager(new LinearLayoutManager((this)));
 
-        recyclerViewUnEnroll = findViewById(R.id.recyclerViewerStudSelectUnEnroll);
-        recyclerViewUnEnroll.setHasFixedSize(true);
-        recyclerViewUnEnroll.setLayoutManager(new LinearLayoutManager((this)));
+        recyclerViewCourseEnrolledIn = findViewById(R.id.recyclerViewerStudCourseEnrolledIn);
+        recyclerViewCourseEnrolledIn.setHasFixedSize(true);
+        recyclerViewCourseEnrolledIn.setLayoutManager(new LinearLayoutManager((this)));
 
         db = db.getInstance();
         mAuth = FirebaseAuth.getInstance();
         courseListNotEnroll = new ArrayList<Course>();
-        courseListEnroll = new ArrayList<Course>();
+        courseListEnrolledIn = new ArrayList<Course>();
 
-        courseAdapterEnroll = new CourseAdapterEnroll(SelectCourseStudentActivity.this, courseListNotEnroll);
-        courseAdapterUnEnroll = new CourseAdapterUnEnroll(SelectCourseStudentActivity.this, courseListEnroll);
+        courseAdapterAvailableCourses = new CourseAdapterEnroll(SelectCourseStudentActivity.this, courseListNotEnroll);
+        courseAdapterCourseEnrolledIn = new CourseAdapterUnEnroll(SelectCourseStudentActivity.this, courseListEnrolledIn);
 
-        recyclerViewEnroll.setAdapter(courseAdapterEnroll);
-        recyclerViewUnEnroll.setAdapter(courseAdapterUnEnroll);
+        recyclerViewAvailableCourses.setAdapter(courseAdapterAvailableCourses);
+        recyclerViewCourseEnrolledIn.setAdapter(courseAdapterCourseEnrolledIn);
+
+        searchText = findViewById(R.id.searchFieldSelectCourseStudent);
+        searchText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchText.setText("");
+            }
+        });
+
+        dayOfWeekSpinner = findViewById(R.id.spinnerSelectCourseStudent);
+        spinnerAdapter = new ArrayAdapter<>(SelectCourseStudentActivity.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.dayOfWeek2));
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dayOfWeekSpinner.setAdapter(spinnerAdapter);
+
+        filterBtn = findViewById(R.id.filterBtnSelectCourseStudent);
+        filterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { searchCourse(); }
+        });
 
         homeBtn = findViewById(R.id.homeBtnStudSelect);
         homeBtn.setOnClickListener(new View.OnClickListener() {
@@ -88,6 +114,9 @@ public class SelectCourseStudentActivity extends AppCompatActivity {
 
         // .get("courseEnroll").isSuccessful()
         Log.d("TESTHERE", (String) userDoc.toString());
+
+        courseListNotEnroll.clear();
+        courseListEnrolledIn.clear();
 
         userDoc.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
@@ -113,7 +142,7 @@ public class SelectCourseStudentActivity extends AppCompatActivity {
                                         Log.d("SUCCESS", document.getId() + " => " + document.getData() + document.toObject(Course.class).getName());
                                         if (courseEnrollList != null) {
                                             if (courseEnrollList.contains(document.getId())) {
-                                                courseListEnroll.add(document.toObject(Course.class));
+                                                courseListEnrolledIn.add(document.toObject(Course.class));
                                             } else {
                                                 courseListNotEnroll.add(document.toObject(Course.class));
                                             }
@@ -122,8 +151,8 @@ public class SelectCourseStudentActivity extends AppCompatActivity {
                                             courseListNotEnroll.add(document.toObject(Course.class));
                                         }
                                     }
-                                    courseAdapterEnroll.notifyDataSetChanged();
-                                    courseAdapterUnEnroll.notifyDataSetChanged();
+                                    courseAdapterAvailableCourses.notifyDataSetChanged();
+                                    courseAdapterCourseEnrolledIn.notifyDataSetChanged();
                                 }
                             }
                         });
@@ -133,28 +162,55 @@ public class SelectCourseStudentActivity extends AppCompatActivity {
                 });
             }
         });
-
-
-
-
-
-
-        //Log.d("TESTHERE", courseEnrollList.toArray().toString());
-
-
-
-//        result = (List<String>) userDoc.get(Source.valueOf(UserField.courseEnroll.toString())).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//            @Override
-//            public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                Log.d("TESTHERE", "Success" );
-//            }
-//        });
-
-
-
     }
 
-    public void openHomePageActivity () {
+    private void searchCourse() {
+        String query = searchText.getText().toString().trim();
+        Log.d("Text", query);
+
+        String courseCodeRegex = "[A-Z]{3}\\s+[0-9]{4}";
+        Pattern courseCodePattern = Pattern.compile(courseCodeRegex);
+        Matcher courseCodeMatch = courseCodePattern.matcher(query);
+        if (courseCodeMatch.matches()) {
+            Log.d("CourseCodeOrName", "Course Code");
+            db.collection("courses").whereEqualTo(CourseField.courseId.toString(), query).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        courseListNotEnroll.clear();
+                        Log.d("CourseCodeOrName", "Course Code Found In FireStore");
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            courseListNotEnroll.add(document.toObject(Course.class));
+                        }
+                        courseAdapterAvailableCourses.notifyDataSetChanged();
+                    }
+                }
+            });
+        }else if (query.equals("") || query.equals("Search")) {
+            Log.d("CourseCodeOrName", "Query Empty");
+            onStart();
+        } else {
+            Log.d("CourseCodeOrName", "Course Name");
+            db.collection("courses").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        courseListNotEnroll.clear();
+                        for (QueryDocumentSnapshot document: task.getResult()) {
+                            Course course = document.toObject(Course.class);
+                            if (course.getName().contains(query)) {
+                                Log.d("CourseCodeOrName", "Course Name Found In FireStore -> " + course.getName() + " vs query "  + query);
+                                courseListNotEnroll.add(course);
+                            }
+                        }
+                        courseAdapterAvailableCourses.notifyDataSetChanged();
+                    }
+                }
+            });
+        }
+    };
+
+    private void openHomePageActivity () {
         Intent intentHomeActivity = new Intent(this, HomeStudentActivity.class);
         startActivity(intentHomeActivity);
     }
